27e73503b0b59f5656552f31ac18a9954cfb7b37
akash7194 <80752954+akash7194@users.noreply.github.com>
2021-06-24 15:39:05 +0530

    added access token and auth

--- Routes/user.routes.js
+++ Routes/user.routes.js
@@ -2,27 +2,39 @@ module.exports = app => {
     var ObjectId = require('mongodb').ObjectID;
     var router = require("express").Router();
     var router1 = require("express").Router();
+    var bcrypt = require('bcrypt')
+    var jwt = require('jsonwebtoken')
+
+    //app.use(express.json())
 
     router.post("/signUp", async (req, res) => {
         try {
-            console.log(req.body);
-            const db = req.app.locals.db;
-            var user = {
-                FirstName: req.body.FirstName,
-                LastName: req.body.LastName,
-                Gender: req.body.Gender,
-                Country: req.body.Country,
-                //store pass encrypted using bcrypt
-                Password: req.body.Password,
-                Email: req.body.Email.trim().toLowerCase(),
-                ConfirmPassword: req.body.ConfirmPassword,
-                Username: req.body.Username,
-                Status: req.body.Status,
-                Timestamp: Date.now(),
-                Follower: [],
-                Following: []
+            if (req.body.Password === req.body.ConfirmPassword) {
+                var salt = await bcrypt.genSalt()
+                console.log(salt);
+                var hashpass = await bcrypt.hash(req.body.Password, salt)
 
-            };
+
+                console.log(req.body);
+                var db = req.app.locals.db;
+                var user = {
+                    FirstName: req.body.FirstName,
+                    LastName: req.body.LastName,
+                    Gender: req.body.Gender,
+                    Country: req.body.Country,
+                    //store pass encrypted using bcrypt
+                    Password: hashpass,
+                    Email: req.body.Email.trim().toLowerCase(),
+                    //confirm is not required to put in db
+                    ConfirmPassword: hashpass,
+                    Username: req.body.Username,
+                    Status: req.body.Status,
+                    Timestamp: Date.now(),
+                    Follower: [],
+                    Following: []
+
+                };
+            }
             if (req.body.Email) {
                 if (user.Password === user.ConfirmPassword) {
                     const users = db.collection('User').insertOne(user).then(results => {
@@ -42,7 +54,7 @@ module.exports = app => {
 
         } catch (error) {
             console.log(error)
-            res.status(500).json({ error: error || "Something went wrong" })
+            res.status(500).json({ error: "error Please check password or username might be in use" || "Something went wrong" })
 
         }
     });
@@ -51,7 +63,7 @@ module.exports = app => {
             console.log(req.body);
             const db = req.app.locals.db;
 
-
+            var userss = undefined;
             var Password = req.body.Password
             if (req.body.Email != undefined)
                 var Email = req.body.Email.trim().toLowerCase()
@@ -61,16 +73,23 @@ module.exports = app => {
 
 
 
-            const users = db.collection('User').findOne({ $or: [{ Email: Email }, { Username: Username }] }).then(results => {
-                if (req.body.Password === results.Password) {
-                    console.log(results)
-                    res.send(results)
-                }
-                else {
-                    res.status(500).json({ error: "Please enter correct password" })
-                }
+            const users = await db.collection('User').findOne({ $or: [{ Email: Email }, { Username: Username }] }).then(results => {
+
+                // res.status(500).json({ error: "No user found" })
+                userss = results;
             })
                 .catch(error => console.log(error))
+            console.log(userss);
+            var condition = await bcrypt.compareSync(req.body.Password, userss.Password);
+            console.log(condition);
+            if (condition) {
+                // console.log(results)
+                const accessToken = jwt.sign(userss, process.env.ACCESS_TOKEN_SECRET)
+                res.send({ success: "login sucess", accessToken: accessToken })
+            }
+            else {
+                res.status(500).json({ error: "Please enter correct password" })
+            }
 
 
         } catch (error) {
@@ -79,6 +98,19 @@ module.exports = app => {
 
         }
     });
+
+    function authenticateToken(req,res,next){
+        const authHeader=req.headers['authorization']
+        const token=authHeader && authHeader.split(' ')[1] 
+        if(token==null) return status(401).json({ error:  "worng token" })
+
+        jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,user) =>{
+            if(err) return status(401).json({ error:  "not access" })
+            req.user=user;
+            console.log(user);
+            next()
+        })
+    }
     router.post("/sendResetLink", async (req, res) => {
         try {
 
@@ -128,10 +160,10 @@ module.exports = app => {
         }
 
     })
-    router1.get("/getPostForUser", async (req, res) => {
+    router1.get("/getPostForUser",authenticateToken, async (req, res) => {
         try {
             const db = req.app.locals.db;
-            const users = db.collection('Posts').find({ PostedBy: req.body.Username }).toArray().then(results => {
+            const users = db.collection('Posts').find({ PostedBy: req.user.Username }).toArray().then(results => {
                 console.log(results)
                 res.send(results)
             })
@@ -178,23 +210,21 @@ module.exports = app => {
                 indicator = 2
             }
             console.log(indicator);
-            if(indicator===2)
-            {
-            const users = db.collection('Posts').find({}).sort({Timestamp : 1}).limit(req.body.Limit).toArray().then(results => {
-                console.log(results)
-                res.send(results)
-            })
-                .catch(error => res.status(200).json({ error: "Something went wrong" }))
-            }
-            else  if(indicator===1)
-            {
-                const users = db.collection('Posts').find({PostedBy : req.body.Username}).sort({Timestamp : 1}).limit(req.body.Limit).toArray().then(results => {
+            if (indicator === 2) {
+                const users = db.collection('Posts').find({}).sort({ Timestamp: 1 }).limit(req.body.Limit).toArray().then(results => {
                     console.log(results)
                     res.send(results)
                 })
                     .catch(error => res.status(200).json({ error: "Something went wrong" }))
             }
-            else{
+            else if (indicator === 1) {
+                const users = db.collection('Posts').find({ PostedBy: req.body.Username }).sort({ Timestamp: 1 }).limit(req.body.Limit).toArray().then(results => {
+                    console.log(results)
+                    res.send(results)
+                })
+                    .catch(error => res.status(200).json({ error: "Something went wrong" }))
+            }
+            else {
                 res.status(200).json({ error: "pass limit and/or username" })
             }
         } catch (error) {
@@ -204,29 +234,28 @@ module.exports = app => {
     })
     router1.post("/likePost", async (req, res) => {
         try {
-            var flag =false;
+            var flag = false;
             const db = req.app.locals.db;
             var Liker = {
-                
+
                 LikedBy: req.body.LikedBy,
-               
+
                 Timestamp: Date.now(),
-               
+
 
             };
-            if(req.body.LikedBy!=undefined)
-            {
-            const users = db.collection('Posts').update({_id : new ObjectId(req.body.id)},{ $inc: { Likes: 1 } ,$push: { "LikedBy": Liker }}).then(results => {
-                console.log(results)
-                res.send(results)
-                flag=true;
-            })
-                .catch(error => res.status(200).json({ error: "Something went wrong" }))
+            if (req.body.LikedBy != undefined) {
+                const users = db.collection('Posts').update({ _id: new ObjectId(req.body.id) }, { $inc: { Likes: 1 }, $push: { "LikedBy": Liker } }).then(results => {
+                    console.log(results)
+                    res.send(results)
+                    flag = true;
+                })
+                    .catch(error => res.status(200).json({ error: "Something went wrong" }))
             }
-            else{
+            else {
                 res.status(200).json({ error: "Something went wrong" })
             }
-              
+
         } catch (error) {
 
         }
---
+++ authserver.js
@@ -0,0 +1,48 @@
+
+const express = require("express");
+const cors = require("cors");
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8080"
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+//Database connection establish
+require("./Routes/user.routes")(app);
+
+const url = 'mongodb://localhost:27017/';
+const dbName = 'qoutesDatabasse';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
+    assert.equal(null, err);
+    console.log('Connected successfully to server');
+
+    const db = client.db(dbName);
+    // storing for acesssing it later
+    app.locals.db = db;
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}.`);
+    });
+
+});
+
+
+
+
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to Qoutes World." });
+});
+
+// set port, listen for requests
